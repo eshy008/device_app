c1a4c99c9df49ca25be15c3a179f939dc513bf2e
eshy008 <eshy008@gmail.com>
2021-06-03 00:33:00 +0100

    intialization of laravel, creating model,controller and route

---
+++ database/migrations/2021_06_02_230916_create_devices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 120);
+            $table->enum("status", [1, 0])->default(1);
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+}
